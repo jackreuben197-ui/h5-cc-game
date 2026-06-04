b1ff11ef0fe35dec74b2a8656d545628bcd575ee
guysoup027 <guysoup027@gmail.com>
2026-06-04 17:19:06 +0800

    fix: bring in toast

--- assets/script/net/messages/texas/BringIn.ts
+++ assets/script/net/messages/texas/BringIn.ts
@@ -1,7 +1,9 @@
 import { createLogger } from '../../../core/decorator/LogTrace';
 import roomDataManager from '../../../data/room/RoomDataManager';
 import TexasGameRoomData from '../../../data/room/texas/TexasGameRoomData';
+import { CPErrorCode } from '../../../i18n/CPErrorCode';
 import { ServerMessageBringIn } from '../../../protobuf/holdem/req_th_bring_in_pb';
+import viewManager from '../../../views/UIViewManager';
 
 const _plog = createLogger('ServerMessageEnterRoom');
 
@@ -10,6 +12,7 @@ export function BringIn(data: ServerMessageBringIn.AsObject, roomID: number, mat
     const roomData = roomDataManager.getRoomData<TexasGameRoomData>(roomID, matchID);
     if (data.status != 0) {
         _plog.error('bring err', data.status);
+        viewManager.showToast(CPErrorCode.ServerErrorDescription(data.status));
         // 你没有选中任何俱乐部
         roomData.mine.currentWalletClubID = 0;
     }
